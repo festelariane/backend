6ba187987778a0edfa12f0aa22cd5e559ee509f2
Jacob Trinh <trinhthaituyen2003@yahoo.com>
2025-08-01 00:45:17 +0700

    Edit meilisearch link.

--- config/plugins.ts
+++ config/plugins.ts
@@ -2,7 +2,7 @@ export default () => ({
     meilisearch: {
     config: {
       // Your meili host
-      host: 'https://search.admin.duhochappytime.edu.vn',
+      host: 'https://search.duhochappytime.edu.vn',
       // Your master key or private key
       apiKey: 'MTj1RK5WnhlQqkP0Z4PrtIKnQF8Xo_xwx2pH8-rDE3U',
     },
